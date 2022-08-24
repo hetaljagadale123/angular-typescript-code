678218fed718ab7d4b21615409aa138e1540402e
hetaljagadale123 <107684347+hetaljagadale123@users.noreply.github.com>
2022-08-24 21:37:24 +0530

    Add files via upload

---
+++ 7_Array.ts
@@ -0,0 +1,21 @@
+
+var arr1:number[];
+arr1 = [1, 2, 3, 4, 5];
+
+var arr2:number[] = [11, 21, 51, 101]; 
+
+var arr3:number[] = new Array(4)  
+arr3 = [1,2,3,4];
+ 
+for(var i = 0; i < arr3.length; i++) 
+{ 
+   console.log(arr3[i]); 
+}
+
+var batches:string[] = new Array("PPA","UNIX","LB","ANGULAR"); 
+
+for(var i = 0;i<batches.length;i++) 
+{ 
+   console.log(batches[i]); 
+}
+
